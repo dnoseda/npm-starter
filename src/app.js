f382b6333019a33feeda567b0f25c16554699bbb
dnoseda <damian.noseda@mercadolibre.com>
2015-10-16 09:42:23 -0300

    working

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
 var grid;
+var $ = require('jquery')
 var Handlebars = require('handlebars')
 var Isotope = require('isotope-layout')
-var grid;
 var MP = require('./mercadolibre-1.0.4')
+
 
 
   MELI.init({client_id: 6586});
 
-  grid = new Isotope('.grid',{
+
+  window.globalGrid =  new Isotope('.grid', {
     // options
     itemSelector: '.grid-item',
     layoutMode: 'fitRows'
   });
 
-  window.grid = grid;
-
   var source   = $("#entry-template").html();
   var template = Handlebars.compile(source);
 
-  $("#submit").on("click",function(){
-    var query = $("#search").val();
+  $("#search").on("click",function(){
+    console.log("clicking");
+    var query = $("#query").val();
     MELI.get('/sites/MLA/search?q='+query, null, function(data) {
       // Your code here
       // var name = data[2]
@@ -28,8 +29,10 @@ var MP = require('./mercadolibre-1.0.4')
       for(var a in data[2].results){
         var item = data[2].results[a];
         var itemHtml = template(item);
-        $grid.isotope('insert',$(itemHtml));
+        //globalGrid.isotope('insert',$(itemHtml));
+        globalGrid.insert($(itemHtml));
       }
     });
   });
+  
 
